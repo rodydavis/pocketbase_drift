2b255cfe695a0afd5be0cc577f2770f2510860ed
Rody Davis <rody.davis.jr@gmail.com>
2023-07-06 08:52:34 -0700

    update

--- lib/src/network/http.dart
+++ lib/src/network/http.dart
@@ -4,14 +4,12 @@ import 'package:http/retry.dart';
 
 class PocketBaseHttpClient extends http.BaseClient {
   final _inner = http.Client();
-  late final _client = RetryClient(_inner, retries: 5);
+  late final _client = RetryClient(_inner, retries: 3);
   static bool offline = false;
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) async {
-    if (offline) {
-      throw Exception('No internet connection');
-    }
+    if (offline) throw Exception('No internet connection');
     debugPrint('request: ${request.method} ${request.url}');
     return _client.send(request);
   }
